080126ce5264d021ce36166d0cdd7725570c0381
Anthony Bloomer <ant0@protonmail.ch>
2018-02-07 00:50:59 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -63,9 +63,14 @@ setup(name='weather-api',
       classifiers=[
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
-          "Topic :: Software Development :: Libraries",
+          'Topic :: Software Development :: Libraries',
+          'Programming Language :: Python',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.6'
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
       ],
       cmdclass={
           'publish': PublishCommand,
