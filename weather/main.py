272cc685d1475390b1d0b53249c47aeb48e2c017
Mitchell Krog <mitchellkrog@gmail.com>
2018-05-02 14:49:25 +0200

    Better formatting

--- weather/main.py
+++ weather/main.py
@@ -4,22 +4,47 @@ import sys
 
 
 def main():
-    pa = ArgumentParser()
-    pa.add_argument('location', help='The location to lookup.')
-    pa.add_argument('--unit', default='c', nargs='?', choices=['c', 'f'])
-    pa.add_argument('--f', '--forecast', help='Pass this argument to get a weather forecast', action="store_true")
-    pa.add_argument('--s', '--start', default='1', nargs='?', help='Start Day')
-    pa.add_argument('--e', '--end', default='3', nargs='?', help='End Day')
-    args = pa.parse_args()
-    weather = Weather(args.unit)
-    loc = weather.lookup_by_location(args.location)
+    PARSER = argparse.ArgumentParser
+    PARSER.add_argument(
+        "location",
+        help="The location to lookup."
+    )
+    PARSER.add_argument(
+        "--unit",
+        default="c",
+        choices=["c","f"],
+        help="Units C or F."
+    )
+    PARSER.add_argument(
+        "--f",
+        "--forecast",
+        action="store_true",
+        help="Pass this argument to get a weather forecast."
+    )
+    PARSER.add_argument(
+        "--s",
+        "--start",
+        default="1",
+        action="store_true",
+        help="Start Day."
+    )
+    PARSER.add_argument(
+        "--e",
+        "--end",
+        default="3",
+        action="store_true",
+        help="End Day."
+    )
+    ARGS = PARSER.parse_args()
+    weather = Weather(ARGS.unit)
+    loc = weather.lookup_by_location(ARGS.location)
     condition = loc.condition
     print("Weather report for %s, %s" % (loc.location.city, loc.location.country))
     print("Condition: %s " % condition.text)
     print("Temperature: %s" % condition.temp)
-if args.forecast:
-   start = 0 if not args.start else args.start # If the start argument isn't set, set the start as the first element in the forecast array.
-   end =  len(location.forecast) if not args.end else args.end # if the end argument isn't set, set the end as the last element in the forecast array.
+if ARGS.forecast:
+   start = 0 if not ARGS.start else ARGS.start # If the start argument isn't set, set the start as the first element in the forecast array.
+   end =  len(location.forecast) if not ARGS.end else ARGS.end # if the end argument isn't set, set the end as the last element in the forecast array.
    for forecast in location.forecast[start:end]:
        print("Day%s: %s" % (start, forecast.text))
        start += 1
