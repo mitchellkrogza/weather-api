46470236d13d84a77895f303bc4a1aeb3778ce3f
Mitchell Krog <mitchellkrog@gmail.com>
2018-05-02 12:56:40 +0200

    Add Forecast Arguments into main.py

--- weather/main.py
+++ weather/main.py
@@ -7,6 +7,9 @@ def main():
     pa = ArgumentParser()
     pa.add_argument('location', help='The location to lookup.')
     pa.add_argument('--unit', default='c', nargs='?', choices=['c', 'f'])
+    pa.add_argument("--f"', "--forecast", nargs="?", "Pass this argument to get a weather forecast")
+    pa.add_argument("--s"', "--start", nargs="?", "The forecast start")
+    pa.add_argument("--e"', "--end", nargs="?", "The forecast end")
     args = pa.parse_args()
     weather = Weather(args.unit)
     loc = weather.lookup_by_location(args.location)
@@ -21,3 +24,11 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         sys.exit(1)
+
+		
+if args.forecast:
+   start = 0 if not args.start else args.start # If the start argument isn't set, set the start as the first element in the forecast array.
+   end =  len(location.forecast) if not args.end else args.end # if the end argument isn't set, set the end as the last element in the forecast array.
+   for forecast in location.forecast[start:end]:
+       print("Day%s: %s" % (start, forecast.text))
+       start += 1
