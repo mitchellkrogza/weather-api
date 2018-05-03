66ef162cdea3d8d6ef923901dd7ea7a78fdb9238
Mitchell Krog <mitchellkrog@gmail.com>
2018-05-03 11:48:41 +0200

    Test print spacing

--- weather/main.py
+++ weather/main.py
@@ -42,7 +42,7 @@ def main():
     print("%s, %s" % (loc.location.city, loc.location.country))
     print("%s" % (condition.date))
     print("-------------------------------------------")
-    print("%s | %s Deg C" % (condition.text, condition.temp))
+    print("%20s | %20s Deg C" % (condition.text, condition.temp))
     print("-------------------------------------------")
 
     if ARGS.forecast:
