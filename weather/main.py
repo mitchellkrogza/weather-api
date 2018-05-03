436123c8301400c35f1063ef279004bc285abb10
Mitchell Krog <mitchellkrog@gmail.com>
2018-05-03 10:29:19 +0200

    Update forecast printing

--- weather/main.py
+++ weather/main.py
@@ -48,8 +48,11 @@ def main():
         start = 0 if not ARGS.start else ARGS.start # If the start argument isn't set, set the start as the first element in the forecast array.
         end =  len(loc.forecast) if not ARGS.end else ARGS.end # if the end argument isn't set, set the end as the last element in the forecast array.
     for forecast in loc.forecast[start:end]:
-        print("Day%s: %s" % (start, forecast.text))
-        print("Temperature: %s" % condition.temp)
+        print("%s: %s" % (forecast.date, forecast.text))
+        print("Temperature: %s" % forecast.high)
+        print("Temperature: %s" % forecast.low)
+        print("Temperature: %s" % forecast.high)
+        print("Temperature: %s" % forecast.day)
         start += 1
 
 if __name__ == '__main__':
@@ -57,3 +60,4 @@ if __name__ == '__main__':
         main()
     except KeyboardInterrupt:
         sys.exit(1)
+
