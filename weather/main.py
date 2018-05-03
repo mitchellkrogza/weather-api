5468ca4b21366b1c99220e608fdd0148ec862782
Mitchell Krog <mitchellkrog@gmail.com>
2018-05-03 12:25:11 +0200

    align

--- weather/main.py
+++ weather/main.py
@@ -41,9 +41,9 @@ def main():
     condition = loc.condition
     print("%s, %s" % (loc.location.city, loc.location.country))
     print("%s" % (condition.date))
-    print("------------------------------------------------")
+    print("----------------------------------------------")
     print("Now:{:^19} | {} Deg C".format(condition.text, condition.temp))
-    print("------------------------------------------------")
+    print("----------------------------------------------")
 
     if ARGS.forecast:
         loc = weather.lookup_by_location(ARGS.location)
@@ -51,8 +51,9 @@ def main():
         start = 0 if not ARGS.start else ARGS.start # If the start argument isn't set, set the start as the first element in the forecast array.
         end =  len(loc.forecast) if not ARGS.end else ARGS.end # if the end argument isn't set, set the end as the last element in the forecast array.
     for forecast in loc.forecast[start:end]:
-        print("{}: {:^18} | High: {} | Low: {}".format(forecast.day, forecast.text, forecast.high, forecast.low))
+        print("{}: {:^18} | High: {} | Low: {} |".format(forecast.day, forecast.text, forecast.high, forecast.low))
         start += 1
+        print("----------------------------------------------")
 
 if __name__ == '__main__':
     try:
