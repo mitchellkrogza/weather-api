427299967046d4caeb0d8eb990ea3ac97940864d
Mitchell Krog <mitchellkrog@gmail.com>
2018-05-03 14:37:15 +0200

    Fix indentation

--- weather/main.py
+++ weather/main.py
@@ -54,9 +54,9 @@ def main():
         condition = loc.condition
         start = 0 if not ARGS.start else ARGS.start # If the start argument isn't set, set the start as the first element in the forecast array.
         end =  len(loc.forecast) if not ARGS.end else ARGS.end # if the end argument isn't set, set the end as the last element in the forecast array.
-    for forecast in loc.forecast[start:end]:
-        print("{}: {:^18} {} | High: {} | Low: {}".format(forecast.day, forecast.text, forecast.code, forecast.high, forecast.low))
-        start += 1
+        for forecast in loc.forecast[start:end]:
+            print("{}: {:^18} {} | High: {} | Low: {}".format(forecast.day, forecast.text, forecast.code, forecast.high, forecast.low))
+            start += 1
 
 if __name__ == '__main__':
     try:
